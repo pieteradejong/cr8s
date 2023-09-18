4c8708f5bfbd5abbd1cc92010a14a1dac7b619a0
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-09-17 22:57:36 -0400

    add models

---
+++ src/models.rs
@@ -0,0 +1,37 @@
+use chrono::NaiveDateTime;
+use Diesel::Insertable;
+use crate::schema::*;
+
+pub struct Rustacean {
+    pub id: i32,
+    pub name: String,
+    pub email: String,
+    pub created_at: NaiveDateTime
+}
+
+#[derive(Insertable)]
+#[diesel(table_name=rustaceans)]
+pub struct NewRustacean {
+    pub name: String,
+    pub email: String
+}
+
+pub struct Crate {
+    pub id: i32,
+    pub rustacean_id: i32,
+    pub code: String,
+    pub name: String,
+    pub version: String,
+    pub description: Option<String>,
+    pub created_at: NaiveDateTime
+}
+
+#[derive(Insertable)]
+#[diesel(table_name=crates)]
+pub struct NewCrate {
+    pub rustacean_id: i32,
+    pub code: String,
+    pub name: String,
+    pub version: String,
+    pub description: Option<String>,
+}
